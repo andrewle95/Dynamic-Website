b8f1240627d71bfc9fcf890a4221ace92b5515cf
andrewle95 <leaa1@uci.edu>
2017-04-24 21:57:32 -0700

    WIP
    WIll make new branch after I commit this

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,51 +2,131 @@ var products = [
   {
     productPicture: '/assets/img/Glass 1.jpeg',
     productName: 'Sunset Glasses',
-    productPrice: '60.00'
+    productPrice: '60.00',
+    additionalDetails: {
+      brand: 'RayBans',
+      color: 'Purple and Yellow Lens',
+      frameMaterial: 'Plastic',
+      lensMaterial: 'Trivex',
+      fit: 'Standard',
+      style: 'Round'
+    }
   },
   {
     productPicture: '/assets/img/Glass 2.jpeg',
     productName: 'Science Lab',
-    productPrice: '69.99'
+    productPrice: '69.99',
+    additionalDetails: {
+      brand: 'Prada',
+      color: 'Red Lens',
+      frameMaterial: 'Titanium',
+      lensMaterial: 'Glass',
+      fit: 'Standard',
+      style: 'Circle'
+    }
   },
   {
     productPicture: '/assets/img/Glass 3.png',
     productName: 'Runner Glasses',
-    productPrice: '79.99'
+    productPrice: '79.99',
+    additionalDetails: {
+      brand: 'Coach',
+      color: 'Pink',
+      frameMaterial: 'Plastic',
+      lensMaterial: 'Plastic',
+      fit: 'Standard',
+      style: 'Round'
+    }
   },
   {
     productPicture: '/assets/img/Glass 4.png',
     productName: 'Beach Paradise',
-    productPrice: '89.99'
+    productPrice: '89.99',
+    additionalDetails: {
+      brand: 'Oakley',
+      color: 'Gray',
+      frameMaterial: 'PVC',
+      lensMaterial: 'Polycarbonate',
+      fit: 'Standard',
+      style: 'Round'
+    }
   },
   {
     productPicture: '/assets/img/Glass 5.jpg',
     productName: 'Protective Glasses',
-    productPrice: '99.99'
+    productPrice: '99.99',
+    additionalDetails: {
+      brand: 'Oakley',
+      color: 'Black',
+      frameMaterial: 'Plastic',
+      lensMaterial: 'Glass',
+      fit: 'Standard',
+      style: 'Square'
+    }
   },
   {
     productPicture: '/assets/img/Glass 6.jpg',
     productName: 'Classic RayBans',
-    productPrice: '120.99'
+    productPrice: '120.99',
+    additionalDetails: {
+      brand: 'RayBans',
+      color: 'Black',
+      frameMaterial: 'Plastic',
+      lensMaterial: 'Plastic',
+      fit: 'Standard',
+      style: 'Rectangular'
+    }
   },
   {
     productPicture: '/assets/img/Glass 7.jpg',
     productName: 'FBI Glasses',
-    productPrice: '75.99'
+    productPrice: '75.99',
+    additionalDetails: {
+      brand: 'Prada',
+      color: 'Black',
+      frameMaterial: 'PVC',
+      lensMaterial: 'Glass',
+      fit: 'Standard',
+      style: 'Round'
+    }
   },
   {
     productPicture: '/assets/img/Glass 8.jpg',
-    productName: 'The Magic School Bus Glasses',
-    productPrice: '86.99'
+    productName: 'Yellow Glasses',
+    productPrice: '86.99',
+    additionalDetails: {
+      brand: 'Oakley',
+      color: 'Yellow',
+      frameMaterial: 'Plastic',
+      lensMaterial: 'Plastic',
+      fit: 'Standard',
+      style: 'Circle'
+    }
   },
   {
     productPicture: '/assets/img/Glass 9.jpg',
     productName: 'Sci-Fi Glasses',
-    productPrice: '45.99'
+    productPrice: '45.99',
+    additionalDetails: {
+      brand: 'Coach',
+      color: 'Yellow Green',
+      frameMaterial: 'Plastic',
+      lensMaterial: 'Glass',
+      fit: 'Standard',
+      style: 'Round'
+    }
   },
   {
     productPicture: '/assets/img/Glass 10.jpg',
     productName: 'Beach Horizon',
-    productPrice: '20.99'
+    productPrice: '20.99',
+    additionalDetails: {
+      brand: 'RayBans',
+      color: 'Blue Orange',
+      frameMaterial: 'Plastic',
+      lensMaterial: 'Plastic',
+      fit: 'Standard',
+      style: 'Square'
+    }
   }
 ]
