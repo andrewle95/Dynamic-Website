724ce2b250fa5a58ff5143a1dbc90729030e9e8c
andrewle95 <leaa1@uci.edu>
2017-04-24 18:56:45 -0700

    Re organized very well

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,51 +1,51 @@
 var products = [
   {
-    productPicture: 'resource/Glass 1.jpeg',
+    productPicture: '/assets/img/Glass 1.jpeg',
     productName: 'Sunset Glasses',
     productPrice: '60.00'
   },
   {
-    productPicture: 'resource/Glass 2.jpeg',
+    productPicture: '/assets/img/Glass 2.jpeg',
     productName: 'Science Lab',
     productPrice: '69.99'
   },
   {
-    productPicture: 'resource/Glass 3.png',
+    productPicture: '/assets/img/Glass 3.png',
     productName: 'Runner Glasses',
     productPrice: '79.99'
   },
   {
-    productPicture: 'resource/Glass 4.png',
+    productPicture: '/assets/img/Glass 4.png',
     productName: 'Beach Paradise',
     productPrice: '89.99'
   },
   {
-    productPicture: 'resource/Glass 5.jpg',
+    productPicture: '/assets/img/Glass 5.jpg',
     productName: 'Protective Glasses',
     productPrice: '99.99'
   },
   {
-    productPicture: 'resource/Glass 6.jpg',
+    productPicture: '/assets/img/Glass 6.jpg',
     productName: 'Classic RayBans',
     productPrice: '120.99'
   },
   {
-    productPicture: 'resource/Glass 7.jpg',
+    productPicture: '/assets/img/Glass 7.jpg',
     productName: 'FBI Glasses',
     productPrice: '75.99'
   },
   {
-    productPicture: 'resource/Glass 8.jpg',
+    productPicture: '/assets/img/Glass 8.jpg',
     productName: 'The Magic School Bus Glasses',
     productPrice: '86.99'
   },
   {
-    productPicture: 'resource/Glass 9.jpg',
+    productPicture: '/assets/img/Glass 9.jpg',
     productName: 'Sci-Fi Glasses',
     productPrice: '45.99'
   },
   {
-    productPicture: 'resource/Glass 10.jpg',
+    productPicture: '/assets/img/Glass 10.jpg',
     productName: 'Beach Horizon',
     productPrice: '20.99'
   }
